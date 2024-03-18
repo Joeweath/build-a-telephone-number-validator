119eb0ac330fff808f6f5aaed478be468fe6772b
Joeweath <jweathingtonsr@gmail.com>
2024-03-18 11:35:38 -0500

    start phone number regex

--- script.js
+++ script.js
@@ -2,11 +2,17 @@ const userInput = document.getElementById('user-input')
 const checkBtn = document.getElementById('check-btn')
 const clearBtn = document.getElementById('clear-btn')
 const resultDiv = document.getElementById('results-div')
+
+const regex = /^1?\s?\(?\d{3}\)?\s?-?\d{3}-?\d{4}$/
+
 checkBtn.addEventListener('click', () => {
     
     if(!userInput.value){
-        resultDiv.textContent = "Please provide a phone number"
+        alert("Please provide a phone number")
     }
+    if(regex.test(userInput.value)){
+        resultDiv.textContent = `Valid US number: ${userInput.value}`
+    } 
 })
 
 clearBtn.addEventListener('click', () => {
