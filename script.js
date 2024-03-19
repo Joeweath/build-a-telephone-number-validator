b86f4a2e0ad8ac949b034815e275a61949ac6f78
Joeweath <jweathingtonsr@gmail.com>
2024-03-19 10:13:15 -0500

    finish phone number regex

--- script.js
+++ script.js
@@ -3,17 +3,25 @@ const checkBtn = document.getElementById('check-btn')
 const clearBtn = document.getElementById('clear-btn')
 const resultDiv = document.getElementById('results-div')
 
-const regex = /^1?\s?\(?\d{3}\)?\s?-?\d{3}-?\d{4}$/
+const regex = /^(1\s?)?(\(\d{3}\)|\d{3})(-|\s)?\d{3}(-|\s)?\d{4}$/;
+
+
+
 
 checkBtn.addEventListener('click', () => {
-    
-    if(!userInput.value){
-        alert("Please provide a phone number")
+    const phoneNumber = userInput.value.trim();
+    console.log("Input phone number:", phoneNumber); 
+    if (!phoneNumber) {
+        alert("Please provide a phone number");
+        return;
     }
-    if(regex.test(userInput.value)){
-        resultDiv.textContent = `Valid US number: ${userInput.value}`
-    } 
-})
+    if (regex.test(phoneNumber)) {
+        resultDiv.textContent = `Valid US number: ${phoneNumber}`;
+    } else {
+        resultDiv.textContent = `Invalid US number: ${phoneNumber}`;
+    }
+});
+
 
 clearBtn.addEventListener('click', () => {
     resultDiv.textContent = ""
